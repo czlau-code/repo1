3710ffc3655081608030ae8ae200131e677959d5
czlau <czlau@outlook.com>
2025-11-07 17:00:44 +0800

    aa

--- test2.py
+++ test2.py
@@ -1,1 +1,7 @@
-print("test2, hello 2 !!!")
+import requests
+
+# requests.adapters.DEFAULT_RETRIES=5
+url = "http://localhost:8000/tclapi/hello2"
+myobj = {"name": "Yoda"}
+x = requests.post(url, json=myobj)
+print(x.text)
---
+++ test2.py1762506004993
@@ -0,0 +1,1 @@
+print("test2, hello 2 !!!")
